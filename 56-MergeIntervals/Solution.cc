40756a7cd825710ee387258ddc28cd351b955ef7
jiaxin96 <572673807@qq.com>
2017-08-15 08:48:51 +0800

    合并区间

---
+++ 56-MergeIntervals/Solution.cc
@@ -0,0 +1,47 @@
+/**
+ * File: e:\code\leetCode\56-MergeIntervals\Solution.cc
+ * Project: e:\code\leetCode
+ * Created Date: Tuesday, August 15th 2017, 1:01:09 am
+ * Author: JX
+ * -----
+ * Last Modified: Tue Aug 15 2017
+ * Modified By: JX
+ * -----
+ * Copyright (c) 2017 SYSU-SDCS-RJX
+ * 
+ * 学习使用
+ * github地址:https://github.com/jiaxin96
+ */
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        vector<Interval> ans;
+        if (intervals.size() <= 0) {
+            return ans;
+        }
+        sort(intervals.begin(), intervals.end(), [=](Interval a, Interval b)->int{if (a.start == b.start) return a.end < b.end; else return a.start < b.start;});
+
+        int s = intervals[0].start, b = intervals[0].end;
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start <= b) {
+                b = max(intervals[i].end, b);
+            } else {
+                ans.push_back(Interval(s,b));
+                s = intervals[i].start;
+                b = intervals[i].end;
+            }
+        }
+        ans.push_back(Interval(s,b));
+        return ans;
+    }
+};
