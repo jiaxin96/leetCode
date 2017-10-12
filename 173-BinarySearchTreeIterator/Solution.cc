fd2811910dbdad6e121e4270b840250422ec9bb5
jiaxin96 <572673807@qq.com>
2017-10-12 09:03:38 +0800

    二叉搜索数迭代器

---
+++ 173-BinarySearchTreeIterator/Solution.cc
@@ -0,0 +1,46 @@
+/**
+ * File: e:\code\leetCode\173-BinarySearchTreeIterator\Solution.cc
+ * Project: e:
+ * Created Date: Thursday, October 12th 2017, 12:28:15 am
+ * Author: JX
+ * -----
+ * Last Modified: Thu Oct 12 2017
+ * Modified By: JX
+ * -----
+ * Copyright (c) 2017 SYSU-SDCS-RJX
+ * 
+ * 学习使用
+ * github地址:https://github.com/jiaxin96
+ */
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root) {
+        
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        
+    }
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
