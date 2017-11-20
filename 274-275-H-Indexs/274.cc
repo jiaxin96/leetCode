1e72f10cbfd484db2637d49e32e2d6ac90e50e9d
jiaxin96 <572673807@qq.com>
2017-11-20 13:49:03 +0800

    index

---
+++ 274-275-H-Indexs/274.cc
@@ -0,0 +1,31 @@
+/**
+ * File: e:\code\leetCode\274-275-H-Indexs\274.cc
+ * Project: e:
+ * Created Date: Monday, November 20th 2017, 11:26:04 am
+ * Author: JX
+ * -----
+ * Last Modified: Mon Nov 20 2017
+ * Modified By: JX
+ * -----
+ * Copyright (c) 2017 SYSU-SDCS-RJX
+ * 
+ * 学习使用
+ * github地址:https://github.com/jiaxin96
+ */
+
+class Solution
+{
+  public:
+    int hIndex(vector<int> &citations)
+    {
+        sort(citations.begin(), citations.end());
+        for (int i = citations.size()-1; i >= 0; --i) {
+            int lef = citations.size() - i;
+            if (citations[i] >= lef)
+            {
+                return lef;
+            }
+        }
+        return 0;
+    }
+};
