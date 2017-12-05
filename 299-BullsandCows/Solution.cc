6776f7851410639ee9695924c64808544acf0de0
jiaxin96 <572673807@qq.com>
2017-12-05 14:10:21 +0800

    公牛母牛没做完

---
+++ 299-BullsandCows/Solution.cc
@@ -0,0 +1,26 @@
+/**
+ * File: e:\code\leetCode\299-BullsandCows\Solution.cc
+ * Project: e:
+ * Created Date: Friday, December 1st 2017, 11:56:53 pm
+ * Author: JX
+ * -----
+ * Last Modified: Sat Dec 02 2017
+ * Modified By: JX
+ * -----
+ * Copyright (c) 2017 SYSU-SDCS-RJX
+ * 
+ * 学习使用
+ * github地址:https://github.com/jiaxin96
+ */
+
+class Solution
+{
+  public:
+    string getHint(string secret, string guess)
+    {
+        vector<int> dit(10,0);
+        for (int i = 0; i < secret.length(); ++i) {
+            
+        }
+    }
+};
